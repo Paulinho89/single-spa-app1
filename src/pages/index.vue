01393ab8c5e154327d36db1f105306837dcec190
曹远鹏 <caoyp@guahao.com>
2021-01-03 18:03:31 +0800

    upd:优化

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='container-children1'>
+        {{ testData }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            testData: '这是子应用1'
+        };
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.container-children1 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 100px;
+    color: blue;
+    font-size: 50px;
+}
+</style>
